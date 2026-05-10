dc10099b12f2beb7e31c30ce628b821c91e098d8
yucekul <alpyucekul@gmail.com>
2026-05-10 14:42:03 +0200

    Add files via upload

---
+++ api/chess.js
@@ -0,0 +1,27 @@
+export default async function handler(req, res) {
+  const response = await fetch(
+    "https://lichess.org/api/challenge/ai",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.LICHESS_TOKEN}`,
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: new URLSearchParams({
+        level: "3",
+        color: "random"
+      })
+    }
+  );
+
+  const data = await response.json();
+
+  if (data.challenge?.url) {
+    res.writeHead(302, {
+      Location: data.challenge.url
+    });
+    res.end();
+  } else {
+    res.status(500).json(data);
+  }
+}
